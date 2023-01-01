d33cffd263f22a6f676be34b542903e8c6a683ec
Scar-Face <peteradaaku@gmail.com>
2023-01-01 21:20:32 +0100

    added the second test

--- test/faucetTests.js
+++ test/faucetTests.js
@@ -11,13 +11,19 @@ describe('Faucet', function () {
 
     const [owner] = await ethers.getSigners();
 
-    console.log('Signer 1 address: ', owner.address);
-    return { faucet, owner };
+    let withdrawAmount = ethers.utils.parseUnits('1', 'ether');
+
+    return { faucet, owner, withdrawAmount };
   }
 
   it('should deploy and set the owner correctly', async function () {
     const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
 
     expect(await faucet.owner()).to.equal(owner.address);
+  });
+
+  it('should not allow withdrawals above .1 ETH at a time', async function () {
+    const { faucet, withdrawAmount } = await loadFixture(deployContractAndSetVariables);
+    await expect(faucet.withdraw(withdrawAmount)).to.be.reverted;
   });
 });
